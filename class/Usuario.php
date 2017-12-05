4e9d071e94a55ffe3a8a1551c1c2b753b5385c7d
jose wilton <josewilltton@gmail.com>
2017-12-05 21:40:37 -0200

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,39 +1,71 @@
 <?php
 
 class Usuario{
-	private $idusuario;
-	private $deslogin;
-	private $dessenha;
-	private $dtcadastro;
+		private $idusuario;
+		private $deslogin;
+		private $dessenha;
+		private $dtcadastro;
 
-	public function getIdusuario(){
-		return $this->idusuario;
+		public function getIdusuario(){
+			return $this->idusuario;
+		}
+		public function getDeslogin(){
+			return $this->deslogin;
+		}
+		public function getDessenha(){
+			return $this->dessenha;
+		}
+		public function getDtcadastro(){
+			return $this->dtcadastro;
+		}
+		public function setIdusuario($value){
+			return $this->idusuario = $value;
+		}
+		public function setDeslogin($value){
+			return $this->deslogin = $value;
+		}
+		public function setDesssenha($value){
+			return $this->dessenha = $value;
+		}
+		public function setDtcadastro($value){
+			return $this->dtcadastro = $value;
+		}
+		public function loadById($id){
+			$sql = new Sql();
+			$results = $sql->select("SELECT * FROM td_usuarios WHERE idusuario = :ID",array(
+				":ID"=>$id
+			));
+			if(count($results)>0){
+				$row = $results[0];
+				$this->setIdusuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDesssenha($row['dessenha']);
+				$this->setDtcadastro(new dateTime($row['dtcadastro']));
+			}
 	}
-	public function getDeslogin(){
-		return $this->deslogin;
+	public function __toString(){
+		return json_encode(array(
+			"idusuario"=>$this->getIdusuario(),
+			"deslogin"=>$this->getDeslogin(),
+			"dessenha"=>$this->getDessenha(),
+			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+		));
 	}
-	public function getDessenha(){
-		return $this->dessenha;
-	}
-	public function getDtcadastro(){
-		return $this->dtcadastro;
-	}
-	public function setIdusuario($value){
-		return $this->idusuario = $value;
-	}
-	public function setDeslogin($value){
-		return $this->deslogin = $value;
-	}
-	public function setDesssenha($value){
-		return $this->dessenha = $value;
-	}
-	public function setDtcadastro($value){
-		return $this->dtcadastro = $value;
-	}
-	public function loadById($id){
+	public static function getUsers(){
 		$sql = new Sql();
-		$results = $sql->select("SELECT * FROM td_usuarios WHERE idusuario = :ID",array(
-			":ID"=>$id
+		return $sql-> select("SELECT *from td_usuarios ORDER BY deslogin");
+	}
+	public static function search($Login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM td_usuarios WHERE deslogin LIKE :procura ORDER BY deslogin",array(
+			":procura" => "%".$Login."%"
+		));
+	}
+	public function login($usuario,$senha){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM td_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD",array(
+			":LOGIN" => $usuario,
+			":PASSWORD" => $senha
 		));
 		if(count($results)>0){
 			$row = $results[0];
@@ -41,16 +73,11 @@ class Usuario{
 			$this->setDeslogin($row['deslogin']);
 			$this->setDesssenha($row['dessenha']);
 			$this->setDtcadastro(new dateTime($row['dtcadastro']));
+		}else{
+			throw new Exception("Login incorreto");
+
 		}
-}
-public function __toString(){
-	return json_encode(array(
-		"idusuario"=>$this->getIdusuario(),
-		"deslogin"=>$this->getDeslogin(),
-		"dessenha"=>$this->getDessenha(),
-		"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-	));
-}
+	}
 }
 
  ?>
--- index.php
+++ index.php
@@ -6,7 +6,27 @@ $usuario = $sql->select("SELECT * FROM td_usuarios");
 $json= json_encode($usuario); // já joga em um json
 print_r($usuario); // funciona normalmente sem passar pelo json.
 */
+/*
+  carregando um registro
 $root = new Usuario();
 $root->loadById(2);// carregando um cadastro pelo id
 echo"<pre>";print_r( $root);
+*/
+/*
+//carrega uma lista de usuarios
+$lista = Usuario::getUsers();
+//exibindo os registros
+echo "<pre>";print_r($lista);
+//exibindo o registro que se encontra na posição 1 do array de objetos
+echo json_encode($lista[1]);
+*/
+//carrega uma lista de usuario buscando pelo login
+/*$busca = Usuario::search("lu");
+echo json_encode($busca);
+*/
+//carrega usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("thiago","2345");
+
+echo"<pre>";print_r($usuario);
 ?>
